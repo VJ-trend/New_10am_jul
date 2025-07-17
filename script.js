df0d8f3a872c3dc60333d4712654e924eab70595
OMR-Trend <nitheesgopi@gmail.com>
2025-07-17 11:41:36 +0530

    First push

---
+++ script.js
@@ -0,0 +1,16 @@
+
+console.log("HEllo world")
+
+
+//git commands  
+
+//push  
+
+//git init -> git empty repo intialize 
+
+//first time 
+
+//git config --global user.email "email@com"
+//git config --global user.name "vj-trend" 
+
+//git add .
